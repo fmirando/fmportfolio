3c45b16938e10a532638db73c1fd15060c6d99dc
Frank <mirandofrank@gmail.com>
2023-08-03 17:56:31 -0700

    Installed packages and dependecies: fonts, sass, animate, etc

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /.(jpe?g|png|gif|svg)$/i,
